1e4d2086371a03cf73b5efdac684234511c9d8b6
Christian Hernandez <christiansv10@hotmail.com>
2021-10-16 14:33:22 -0600

    Agregando selectores

--- src/components/CrearSolicitud.js
+++ src/components/CrearSolicitud.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FormGroup, FormControl, InputLabel, Input, Button, makeStyles, Typography } from '@material-ui/core';
-//import { addSolicitud } from '../config/axios';
+import { addSolicitud } from '../config/axios';
 import { useHistory } from "react-router-dom";
 
 const initialValue = {
@@ -8,6 +8,10 @@ const initialValue = {
     fechayHoraVisita: '',
     motivo: '',
     idArea: '',
+    sintomas: '',
+    diagnosticado: '',
+    covidFamiliar: '',
+    viajo: ''
 }
 
 const useStyles = makeStyles({
@@ -22,10 +26,10 @@ const useStyles = makeStyles({
 
 const CrearSolicitud = () => {
     const [solicitud, setSolicitud] = useState(initialValue);
-    const { idUsuario, fechayHoraVisita, motivo, idArea } = solicitud;
+    const { idUsuario, fechayHoraVisita, motivo, idArea, sintomas, diagnosticado, covidFamiliar, viajo } = solicitud;
     const classes = useStyles();
 
-    //const history = useHistory();
+    const history = useHistory();
     
     const onValueChange = (e) => {
         setSolicitud({...solicitud, [e.target.name]: e.target.value})
@@ -39,9 +43,10 @@ const CrearSolicitud = () => {
         } else if(idArea.trim() === ""){
             alert("Area requerida")
         } else {
-            /* await addSolicitud(solicitud);
+            console.log(solicitud);
+            /*await addSolicitud(solicitud);
             alert('Solicitud agregada');
-            history.push('./adminhome'); */
+            history.push('./adminhome');*/
         }
     }
 
@@ -64,6 +69,34 @@ const CrearSolicitud = () => {
             <FormControl>
                 <InputLabel htmlFor="my-input">idArea</InputLabel>
                 <Input onChange={(e) => onValueChange(e)} name='idArea' value={idArea} id="my-input" />
+            </FormControl>
+            <FormControl>
+            <InputLabel htmlFor="my-input">Ha presentado sintomas de fibre, gripe, tos o dolor de cuerpo durante ultimamente?</InputLabel>
+                <select onChange={(e) => onValueChange(e)} name="sintomas" id="sintomas">
+                    <option value={sintomas}>Si</option>
+                    <option value={sintomas}>No</option>
+                </select>
+            </FormControl>
+            <FormControl>
+            <InputLabel htmlFor="my-input">Ha sido diagnosticado o ha presentado sospechas de COVID-19?</InputLabel>
+                <select onChange={(e) => onValueChange(e)} name="diagnosticado" id="diagnosticado">
+                    <option value={diagnosticado}>Si</option>
+                    <option value={diagnosticado}>No</option>
+                </select>
+            </FormControl>
+            <FormControl>
+            <InputLabel htmlFor="my-input">Tiene familiares que hayan sido diagnosticados por COVID-19?</InputLabel>
+                <select onChange={(e) => onValueChange(e)} name="covidFamiliar" id="covidFamiliar">
+                    <option value={covidFamiliar}>Si</option>
+                    <option value={covidFamiliar}>No</option>
+                </select>
+            </FormControl>
+            <FormControl>
+            <InputLabel htmlFor="my-input">Ha salido del pais durante los pasados 15 dias?</InputLabel>
+                <select onChange={(e) => onValueChange(e)} name="viajo" id="viajo">
+                    <option value={viajo}>Si</option>
+                    <option value={viajo}>No</option>
+                </select>
             </FormControl>
             <FormControl>
                 <Button variant="contained" color="primary" onClick={() => addSol()}>Agregar usuario</Button>
